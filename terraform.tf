5419c4f217d9634130193a7765b7263cfbeb3bea
grant <grant.moore@maryland.gov>
2024-06-29 10:29:23 -0400

    remove unnecessary providers

--- terraform.tf
+++ terraform.tf
@@ -5,15 +5,6 @@ terraform {
     aws                     = {
       source                = "hashicorp/aws"
       version               = ">= 4.8.0"
-      configuration_aliases = [ aws.core ]
-    }
-    kubernetes              = {
-      source                = "hashicorp/kubernetes"
-      version               = "2.31.0"
-    }
-    tls                     = {
-      source                = "hashicorp/tls"
-      version               = "4.0.5"
     }
   }
 }
