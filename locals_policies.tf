4bda57ce6c9968c185d57da80742b4b5aec09506
grant <grant.moore@maryland.gov>
2024-06-30 10:30:18 -0400

    use platform module

--- locals_policies.tf
+++ locals_policies.tf
@@ -1,9 +1,10 @@
 locals {
     platform_templatefile_vars  = {
-
+        # TODO: in case we ever have to template policies!
     }
     platform_policy_fileset     = fileset("${path.module}/policies/platform", "*.tftpl")
     platform_policies           = {
+        # take all policies in policies/platform directory
         for index, file_path in local.platform_policy_fileset:
             # map filename without file extension to templated policy string
             split(".", file_path)[0] => 
