23612b5c6c90a5d9c7c041fa6db9740771835141
grant <grant.moore@maryland.gov>
2024-06-30 17:16:19 -0400

    update platform mod calls

--- resources_policies.tf
+++ resources_policies.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy" "platform" {
     for_each    = local.platform_policies
 
-    name        = "${module.platform.prefixes.identity.policy}-${each.key}"
+    name        = "${module.platform.prefixes.identity.iam.policy}-${each.key}"
     path        = "/platform/"
     policy      = each.value
 }
