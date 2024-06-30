5c06f55241c44537e420c7b26ae9131b7e22a580
grant <grant.moore@maryland.gov>
2024-06-30 17:28:59 -0400

    update platform calls

--- locals.tf
+++ locals.tf
@@ -3,16 +3,9 @@ locals {
     ## CALCULATED PROPERTIES
     # Variables that store local calculations that change based on the 
     #   the deployment configuration.
-    tags                        = {
-        Owner                   = var.iam_config.tags.owner
-        Account                 = module.platform.account.threeletterkey
-        Agency                  = module.platform.agency.abbr
-        Program                 = module.platform.program.key
-        Purpose                 = var.iam_config.tags.purpose
-        "PCA Code"              = var.platform.pca
-        CreationDate            = formatdate("YYYY-MM-DD", timestamp())
+    tags                        = merge({
         PrimaryContact          = var.iam_config.tags.contact
         CrossAccountRole        = var.iam_config.tags.cross_account_role
         AutoCleanup             = var.iam_config.tags.auto_cleanup
-    }
+    }, module.platform.tags)
 }
