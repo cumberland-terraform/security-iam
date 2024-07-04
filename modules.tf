584fd08e231a525cd04dfc48c9d47a656fa739d9
grant <grant.moore@maryland.gov>
2024-07-04 15:24:49 -0400

    add comments

--- modules.tf
+++ modules.tf
@@ -1,5 +1,5 @@
 module "platform" {
   source                = "git::ssh://git@source.mdthink.maryland.gov:22/et/mdt-eter-platform.git"
   
-  lookup                = var.platform
+  platform              = var.platform
 }
